28cb2c392c947d3132dd1ff5fce692574bc394a4
Jailson <97315804+JaPCarriel@users.noreply.github.com>
2022-09-05 18:15:30 -0300

    Update script_motor_cc.m

--- script_motor_cc.m
+++ script_motor_cc.m
@@ -1,26 +1,32 @@
-%% Script pra simular o motor CC com controle PWM
+%% Script to simulate the DC Motor with a PWM controller
+%% Script para simular o motor CC com controle PWM
 
-% Declarando os par‚metros do Motor
-If = 1; % Corrente de Campo - A
-La = 0.0025; %Indut‚ncia da Armadura - H
-Ra = 0.5; %ResistÍncia da Armadura - Ohms
-Vn = 120; % Tens„o Nominal - V
-In = 20;  % Corrente Nominal - A
-n = 3000; % Velocidade Nominal - rpm
+% Motor parameters (value; parameter - Unit)
+% Declarando os par√¢metros do Motor
+If = 1; % Field Current - A | Corrente de Campo - A
+La = 0.0025; % Armature Inductance - H | Indut√¢ncia da Armadura - H
+Ra = 0.5; % Armature Resistance - Ohms | Resist√™ncia da Armadura - Ohms
+Vn = 120; % Nomival Voltage - V | Tens√£o Nominal - V
+In = 20;  % Nominal Current - A | Corrente Nominal - A
+n = 3000; % Nominal Velocity - rpm | Velocidade Nominal - rpm
 
-% Par‚metros da Carga
-J = 0.001; %kg.m≤
-M = (Vn - Ra*In)/(2*pi*n/60)/If
-K1 = (M*If*In)/(2*pi*n/60)
+% Load Parameters
+% Par√¢metros da Carga
+J = 0.001; % Servo Motor Inertia - kg.m¬≤ | Momento de Inercia no Servo motor - kg.m¬≤ 
+M = (Vn - Ra*In)/(2*pi*n/60)/If %A constant related to the motor Geometry.
+K1 = (M*If*In)/(2*pi*n/60) %Torque - N.m
 
-% Par‚metros do tempo
-w_desejado = [0 n n 0 0 -n -n 0 0].';
-t = [0 0.1 0.5 0.6 0.7 1.0 1.2 1.21 1.5].';
+% Time parameters
+% Par√¢metros do tempo
+w_desejado = [0 n n 0 0 -n -n 0 0].'; %% w expected
+t = [0 0.1 0.5 0.6 0.7 1.0 1.2 1.21 1.5].'; %% time
 
-% Par‚metros PWM
+% PWM parameters
+% Par√¢metros PWM
 Vpwm = 565; % V
 
-% Par‚metros PID
+% PID parameters
+% Par√¢metros PID
 p = 10;
 i = 50;
 d = 0;
